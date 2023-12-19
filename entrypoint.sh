fedd5546ee04b0fb950ff86d590bea775114b5bc
KilbackClifton <154320969+KilbackClifton@users.noreply.github.com>
2023-12-19 17:24:40 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Web Site:https://www.hicairo.com
 # Telegram:https://t.me/HiaiFeng
 
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
-sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
+sed -i "s#UUID#$6dedcd87-25ae-482f-8756-0b968052e85f#g;s#VMESS_WSPATH#$6dedcd87-25ae-482f-8756-0b968052e85f-vmess#g;s#VLESS_WSPATH#$6dedcd87-25ae-482f-8756-0b968052e85f-vless#g" /etc/v2ray/config.json
+sed -i "s#VMESS_WSPATH#$6dedcd87-25ae-482f-8756-0b968052e85f-vmess#g;s#VLESS_WSPATH#$6dedcd87-25ae-482f-8756-0b968052e85f-vless#g" /etc/nginx/nginx.conf
 # exec "$@"
 
 # 请删掉下一行最前面的 # 号，同时将这行代码中的域名替换成 Replit 为你项目分配的域名。
